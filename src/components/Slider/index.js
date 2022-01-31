4d7e881e724dfb935f53958d74e754d12cdd40db
Filipe <filipepachecogomes2006@gmail.com>
2022-01-31 17:37:07 -0300

    add layout slides

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 
-import { ContainerSlider, Slides } from './styled';
+import { ContainerSlider, Slides, ChevronRight, ContainerChevronRight } from './styled';
 
 export default function Slider() {
   return (
     <ContainerSlider>
       <Slides>
         <img
-          src="https://occ-0-4762-559.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVgaDddSQkAYCw_KqP3t49pkSKLjhSN84-o8TYhwP_x8WVvA21M3MZ77XNzE5bIgBzfUXT_hlgxyUCJn-mH74NNiEdJdxTF5ZFAf_y6wFCbxgvZ8_tVp6LrLEfipDXEtUMxoOk8jqfEhi6zaGy3fEsvMT1hC5w.jpg?r=6b9"
+          src="https://occ-0-4301-559.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbByeysdEXPBlI4Rat7dUHU2IdnxkJ33pIk1RCsaaaJhSwK882wZxbyA_J1shwyeML8fx0Cs9zHK6ri4RaAQFa9Ta6IdFbTHvhbfiSo087ahtyu0dim6vrrP6BMZtZZRvRgpoAgjCy5SYb4aFACzgUA7cK_xsQ.jpg?r=406"
           alt="Expresso do amanhã"
         />
         <img
-          src="https://occ-0-4762-559.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABfQalv_7KjI06SnsqA0UOAdslnWZXAlf5b8EDsdzqYTUxWt4TJGBOmhbCBd0E_oeUHNliwsOtHdoNc5wLDT5MtjorXqAgPXBMtPNAFaYhINifgagkRNsMC-5jnqcPjvKvW9I-sPuH_C55DjEvQ9dSpmoH2h1bOd51hfEOrJ9GvUlSM4vqiu7pv09LWTAEx-NhXPl5VVrvwSJxqt29DH2B_hR.jpg?r=1be"
+          src="https://occ-0-4301-559.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABe7sMbQh54LR2Sbt6FyDt4RLY1igY8X1Ofw_GIvUuWr38w-bzk6Lp0444INcU9R_q8Mi51gyS0nqvoli44kQEhMHe2WyQJmFAaBsLwmvBAhS6ZBfu6DR5uzX5CuwiP7u8x_AFs3M2yGpOvC5S9D9gAI6nrezMsQWg0JjbYA6N0Vf4yjkeEG0kobH8SsrAhDnFYDMfDR5Vu_VfBIgWj5LJT6p.jpg?r=61a"
           alt="Ozark"
         />
+        <img
+          src="https://occ-0-4301-559.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABY4guFxuW4aavSWWOm_zhjk03Vz7SDYBuWCRe3oUB4UVtVdY4Ziti_N3OIfM43xCRsQDo5isUJ_4zNW8VNfscAnlhIjUw7DVL35cQNp2N2ohLstRo6N-1fSMZOtQ6ARIRpdi921CjkD2WwMbuebttBgqxVtfPAFhnWQekw-_P3z73IXS9DW3DoI.jpg?r=806"
+          alt="Neymar"
+        />
+        <img
+          src="https://occ-0-4301-559.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZSG6deJUNcFiwHugigabPyi5I-GwcB-RZ0bgggIzR5NX5l0lB-GPgYojYqFmbUjAWDPJqs0HK4A8m8uu1gDjwBMrY9SaI_nwSZODsapoapntQSgqlXT_WhC2hwM.jpg?r=e19"
+          alt="Arquivo81"
+        />
+        <img
+          src="https://occ-0-4301-559.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZxIiMMupr4VUlZPgFRVU9vjl1JywA5WR4CzuPDrqHLMSXB0Zm1u4_w0kfAV0BDfbC06PpeVjsKJMvERbExvG_snU53tx_78WalAZlFp8H5w4Bl2_vrnq9F3QCAijQgqx35sHJV9974Os_iLx95ayft61BYJVg.jpg?r=cea"
+          alt="AfterLife"
+        />
+        <ContainerChevronRight>
+          <ChevronRight type="button">asd</ChevronRight>
+        </ContainerChevronRight>
       </Slides>
     </ContainerSlider>
   );
--- src/components/Slider/styled.js
+++ src/components/Slider/styled.js
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
+import { FaChevronRight } from 'react-icons/fa';
+
 export const ContainerSlider = styled.div`
   height: 100%;
+`;
+
+export const ChevronRight = styled(FaChevronRight)`
+  font-size: 2.5vw;
+  display: none;
 `;
 
 export const Slides = styled.div`
@@ -10,5 +17,32 @@ export const Slides = styled.div`
   padding: 0 4%;
   img {
     width: 20%;
+    border-radius: 3px;
+    padding-right: 6px;
+  }
+`;
+
+export const ContainerChevronRight = styled.span`
+  background: rgba(20, 20, 20, 0.5);
+  position: absolute;
+  z-index: 20;
+  width: 4%;
+  right: 0;
+  height: 21.82%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+
+  :hover {
+    cursor: pointer;
+    background: rgba(20, 20, 20, 0.7);
+    ${ChevronRight} {
+      display: flex;
+      transition: transform 0.1s ease-out 0s, -webkit-transform 0.1s ease-out 0s, -moz-transform 0.1s ease-out 0s,
+        -o-transform 0.1s ease-out 0s;
+    }
   }
 `;
